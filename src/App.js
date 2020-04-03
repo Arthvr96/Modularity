3f3ed951485f792a38666f4871dc71068a437363
Artur Chmielewski <arturchmielewski@MacBook-Pro-Artur.local>
2020-04-03 17:53:15 +0200

    Config husky and lint-staged

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          psum et ea dolor duo sit ffffffffffffffffffffffffffffffffffaliquyam
-          rebum ipsum, no lorem at fbfbsit ut sed stedtffaccusam ut dolores.
-          Sitff justo sed erat duo dolore rebum tempor justo sea. Ipdsum eoffs
-          gubergren voluptua tempor et ea. Rebufhhdfm fea dolore est sit. Effff
-          sea sit elitr sit dolore et nonumy diam.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => <h1>Hello World</h1>;
 
 export default App;
